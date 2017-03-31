cbbe8f12ed637eafb9a10088818f1c3625c169c5
krichdev <kgrichardson2@gmail.com>
2017-03-31 15:57:23 -0700

    index.js tweak on server listen

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
 
-server.listen(process.env.PORT || 3000)
+

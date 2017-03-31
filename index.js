139c8b4bd9e886cff49fef7470cae8e7a19e08bb
krichdev <kgrichardson2@gmail.com>
2017-03-31 16:48:35 -0700

    heroku test fix

--- index.js
+++ index.js
@@ -113,9 +113,9 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000);
+// server.listen(process.env.PORT || 3000)
 
 module.exports = server;
 
-server.listen(process.env.PORT || 3000)
 
